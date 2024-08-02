86e0ff92af7118a4f99772a5ea7e260a46180b1a
Mo Charrout <mocharrout@gmail.com>
2024-08-02 19:47:17 +0200

    command only inserts macro

--- src/logseq/command.ts
+++ src/logseq/command.ts
@@ -2,12 +2,21 @@ export async function onCreateIbCommand({ uuid }: { uuid: string }) {
 	const block = await logseq.Editor.getBlock(uuid);
 	if (!block) return;
 
-	let props = new Map<string, any>([
-		['ib-a', block.properties?.ibA ?? 1.0],
-		['ib-b', block.properties?.ibB ?? 1.0],
-	]);
-	console.log(props);
-	for (let [prop, val] of props) {
-		await logseq.Editor.upsertBlockProperty(block.uuid, prop, val);
-	}
+  // Don't do anything if already contains macro.
+  const rendererMacro = '{{renderer :ib}}';
+  if (block.content.includes(rendererMacro)) return;
+
+  // This returns the cursor back to original position, but doesn't
+  // work reliably.
+	// const blockPos = await logseq.Editor.getEditingCursorPosition();
+  // await logseq.Editor.updateBlock(uuid, block.content + `\n${rendererMacro}`);
+  // if (blockPos) {
+  //   setTimeout(async () => { 
+  //     await logseq.Editor.editBlock(uuid, { pos: blockPos.pos });
+  //   }, 200);
+  // }
+
+  // Add macro.
+  await logseq.Editor.updateBlock(uuid, block.content + `\n${rendererMacro}`);
+  await logseq.Editor.exitEditingMode();
 }
