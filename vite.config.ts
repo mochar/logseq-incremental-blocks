bb2627509b0ef93481798449689f342b3c6cbc0a
Mo Charrout <mocharrout@gmail.com>
2024-09-20 16:22:10 +0200

    svgr stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import reactPlugin from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import logseqDevPlugin from "vite-plugin-logseq";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [logseqDevPlugin(), reactPlugin()],
+  plugins: [logseqDevPlugin(), reactPlugin(), svgr()],
   // Makes HMR available for development
   build: {
     target: "esnext",
